79ff2e324391f17f057d6da3bb81110a287ef3ee
ilya-baykov <i-baykov@internet.ru>
2023-02-07 02:40:32 +0300

    Вторая версия решения задачи

--- 1.py
+++ 1.py
@@ -1,15 +1,33 @@
 import requests
-import re
 
-# YES
-# https://stepic.org/media/attachments/lesson/24472/sample0.html
-# https://stepic.org/media/attachments/lesson/24472/sample1.html
-# https://stepic.org/media/attachments/lesson/24472/sample2.html
-link_A = input()
-link_B = input()
-link_C = requests.get(link_A).text.split('"')[1]
-link_C_returns = requests.get(link_C).text.split('"')[1]
-if link_C_returns == link_B:
-    print("Yes")
-else:
-    print("No")
+
+class Link_scanner:
+    def __init__(self, link_A, link_B):
+        self.__link_A = link_A
+        self.__link_B = link_B
+
+    @staticmethod
+    def link_list_gen(current_link: str) -> list[str]:
+        return [link for link in requests.get(current_link).text.split('"') if link.startswith("https:")]
+
+    def equals_link(self) -> str:
+        for link in self.link_list_gen(self.__link_A):
+            for link_inners in self.link_list_gen(link):
+                if link_inners == self.__link_B:
+                    return "Yes"
+            return "No"
+
+
+test_1 = Link_scanner("https://stepic.org/media/attachments/lesson/24472/sample0.html",
+                      "https://stepic.org/media/attachments/lesson/24472/sample2.html")
+
+test_2 = Link_scanner("https://stepic.org/media/attachments/lesson/24472/sample0.html",
+                      "https://stepic.org/media/attachments/lesson/24472/sample1.html")
+
+test_3 = Link_scanner("https://stepic.org/media/attachments/lesson/24472/sample1.html",
+                      "https://stepic.org/media/attachments/lesson/24472/sample2.html")
+
+if __name__ == '__main__':
+    print(test_1.equals_link())
+    print(test_2.equals_link())
+    print(test_3.equals_link())
